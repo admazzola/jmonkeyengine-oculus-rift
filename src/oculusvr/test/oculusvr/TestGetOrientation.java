d5fc5913da2af32795f73f0909985665e96a6021
Andrew Mazzola <admazzola@gmail.com>
2015-06-24 15:50:53 -0400

    Imported into eclipse and cleaned up

---
+++ src/oculusvr/test/oculusvr/TestGetOrientation.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oculusvr.test.oculusvr;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.math.Quaternion;
+import oculusvr.input.OculusRift;
+
+/**
+ *
+ * @author Rickard
+ */
+public class TestGetOrientation extends SimpleApplication {
+
+    public static void main(String[] args) {
+        OculusRift.initialize();
+        TestGetOrientation app = new TestGetOrientation();
+        app.start();
+
+    }
+
+    @Override
+    public void simpleInitApp() {
+        
+        OculusRift.getHMDInfo();
+        Quaternion quat = OculusRift.getOrientation();
+        System.out.println(quat.toString());
+        OculusRift.destroy();
+    }
+}
